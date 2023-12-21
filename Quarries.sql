3a3ba94ca4099a99976a23fac6dbd36088ba0f72
MinyMaLi <153743250+MinyMaLi@users.noreply.github.com>
2023-12-21 23:38:01 +0100

    Update Quarries.sql

--- Quarries.sql
+++ Quarries.sql
@@ -1,54 +1,43 @@
+-- Get's comments made on videos
+
 SELECT c.comment_text, u.username, v.video_name
 FROM public."Comment" c
 JOIN public."User" u ON c.user_id = u.user_id
 JOIN public."Video" v ON c.video_id = v.video_id;
 
+--Users who have not commented on anything
 SELECT u.username
 FROM public."User" u
 LEFT JOIN public."Comment" c ON u.user_id = c.user_id
 WHERE c.commnet_id IS NULL;
 
+--Gets video with "random" in it's name
 SELECT *
-FROM public."Comment"
-WHERE comment_text LIKE '%important%';
+FROM public."Video"
+WHERE video_name LIKE '%Random%';
 
-SELECT u.username
-FROM public."User" u
-WHERE u.user_id = ALL (
-    SELECT c.user_id
-    FROM public."Comment" c
-    JOIN public."Video" v ON c.video_id = v.video_id
+--Videos that ware viewed by every user
+SELECT v.*
+FROM public."Video" v
+WHERE v.video_id = ALL (
+    SELECT v2.video_id
+    FROM public."User" u
+    JOIN public."View" v2 ON u.user_id = v2.user_id
+    WHERE v2.video_id = v.video_id
 );
 
+-- Gets Comment with the user who made it and also the video it was made on
 SELECT c.comment_text, u.username, v.video_name
 FROM public."Comment" c
 JOIN public."User" u ON c.user_id = u.user_id
 JOIN public."Video" v ON c.video_id = v.video_id;
 
+-- Gets all the comments with the user data 
 SELECT *
 FROM public."User"
 JOIN public."Comment" USING (user_id);
 
-SELECT *
-FROM public."User"
-CROSS JOIN public."Video";
-
-SELECT u.username, v.video_name
-FROM public."User" u
-CROSS JOIN public."Video" v;
-
+-- Gets the comments based on users who made them
 SELECT *
 FROM public."User" u
-LEFT JOIN public."Comment" c ON u.user_id = c.user_id;
-
-SELECT *
-FROM table1
-FULL OUTER JOIN table2 ON table1.column = table2.column;
-
-SELECT *
-FROM public."User" u
-WHERE EXISTS (
-    SELECT 1
-    FROM public."Comment" c
-    WHERE c.user_id = u.user_id
-);
+RIGHT JOIN public."Comment" c ON u.user_id = c.user_id;
