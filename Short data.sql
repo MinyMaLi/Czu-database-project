9b311b9dce26025f06a5ff1e0f4b683002ac13c9
MinyMaLi <153743250+MinyMaLi@users.noreply.github.com>
2023-12-22 14:05:45 +0100

    new data

--- Short data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public."Short"(short_id,description_id,user_id,short_name,short_upload_date,views,likes) VALUES (1,6,6,'League of legends A pentakill that will blow your mind','4/16/2018',9614,933);
-INSERT INTO public."Short"(short_id,description_id,user_id,short_name,short_upload_date,views,likes) VALUES (2,7,7,'Poop ASMR','3/13/2014',1784,298);
-INSERT INTO public."Short"(short_id,description_id,user_id,short_name,short_upload_date,views,likes) VALUES (3,8,8,'Coding 101','5/19/2021',8146,711);
-INSERT INTO public."Short"(short_id,description_id,user_id,short_name,short_upload_date,views,likes) VALUES (4,9,9,'Yoga','7/10/2021',2350,504);
-INSERT INTO public."Short"(short_id,description_id,user_id,short_name,short_upload_date,views,likes) VALUES (5,10,10,'dsbd23','3/25/2021',804,380);
