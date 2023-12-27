1ad5ea0615d5d52b1b030a6dd3930e7ffb611718
MinyMaLi <153743250+MinyMaLi@users.noreply.github.com>
2023-12-27 19:40:58 +0100

    update

--- Playlist data.sql
+++ Playlist data.sql
@@ -1,5 +1,5 @@
-INSERT INTO public."Playlist"(playlist_id,user_id,video_id,short_id,name) VALUES (1,3,3,NULL,'music');
-INSERT INTO public."Playlist"(playlist_id,user_id,video_id,short_id,name) VALUES (2,4,4,NULL,'playlist1');
-INSERT INTO public."Playlist"(playlist_id,user_id,video_id,short_id,name) VALUES (3,2,4,NULL,'playlsit2');
-INSERT INTO public."Playlist"(playlist_id,user_id,video_id,short_id,name) VALUES (4,2,4,NULL,'digital');
-INSERT INTO public."Playlist"(playlist_id,user_id,video_id,short_id,name) VALUES (5,1,NULL,2,'nonsense');
+INSERT INTO public."Playlist"(playlist_id,user_id,video_id,name) VALUES (1,3,3,'music');
+INSERT INTO public."Playlist"(playlist_id,user_id,video_id,name) VALUES (2,4,4,'playlist1');
+INSERT INTO public."Playlist"(playlist_id,user_id,video_id,name) VALUES (3,2,4,'playlsit2');
+INSERT INTO public."Playlist"(playlist_id,user_id,video_id,name) VALUES (4,2,4,'digital');
+INSERT INTO public."Playlist"(playlist_id,user_id,video_id,name) VALUES (5,1,2,'nonsense');
